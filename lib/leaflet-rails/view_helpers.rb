ceeb089468d3bffe3c4788196a4f2af4bcc76145
Nick Romanowski <nick@paweblabs.com>
2017-05-12 15:29:42 -0400

    Add fit to markers

--- lib/leaflet-rails/view_helpers.rb
+++ lib/leaflet-rails/view_helpers.rb
@@ -54,6 +54,12 @@ module Leaflet
         }).addTo(map);"
         end
       end
+      
+      if options[:fit_to_markers] && options[:markers] && (options[:markers].count > 1)
+        locations = options[:markers].collect { |m| [m[:latlng][0].to_f, m[:latlng][1].to_f]  }
+
+        output << "map.fitBounds( L.latLngBounds( #{locations} ) );"
+      end
 
       if polylines
          polylines.each do |polyline|
